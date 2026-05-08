1375046ee59ee781d668d0aa9ca2356f8bd350d5
fusiontechindustries <fusiontechindustriesllc@gmail.com>
2026-05-08 08:26:04 -0500

    debug: add error handling to functions entry point

--- functions/api/[[route]].js
+++ functions/api/[[route]].js
@@ -1,5 +1,16 @@
 import app from "../../worker/index.js";
 
-export const onRequest = (context) => {
-  return app.fetch(context.request, context.env, context);
+export const onRequest = async (context) => {
+  try {
+    return await app.fetch(context.request, context.env, context);
+  } catch (err) {
+    return new Response(JSON.stringify({ 
+      error: err.message, 
+      stack: err.stack,
+      env_keys: Object.keys(context.env || {})
+    }), { 
+      status: 500,
+      headers: { 'Content-Type': 'application/json' }
+    });
+  }
 };
