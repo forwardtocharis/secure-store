b2ded0da19fd72b735c54510e834d4a2d492a063
fusiontechindustries <fusiontechindustriesllc@gmail.com>
2026-05-08 10:52:56 -0500

    Configure: Update Capacitor config for Passkeys

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,13 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.forwardtocharis.securestore',
   appName: 'SecureStore',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {
+    CapacitorPasskey: {
+      origin: 'https://secure-store.pages.dev',
+      domains: ['secure-store.pages.dev']
+    }
+  }
 };
 
 export default config;
